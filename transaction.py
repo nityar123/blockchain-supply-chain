190f18615abd84c74e71e4333c5bd06af462c391
Nitya <143220119+nityar123@users.noreply.github.com>
2025-01-23 19:13:39 -0500

    Create transaction.py

---
+++ transaction.py
@@ -0,0 +1,5 @@
+class Transaction:
+    def __init__(self, sender, receiver, product_details):
+        self.sender = sender
+        self.receiver = receiver
+        self.product_details = product_details
